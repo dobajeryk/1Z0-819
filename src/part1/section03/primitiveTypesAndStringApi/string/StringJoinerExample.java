1ab39c9ae10264be4a0b8a73395b3403308dff9b
Eryk Dobaj <dobajeryk@gmail.com>
2021-02-15 17:12:00 +0100

    Added string package

---
+++ src/part1/section03/primitiveTypesAndStringApi/string/StringJoinerExample.java
@@ -0,0 +1,20 @@
+package part1.section03.primitiveTypesAndStringApi.string;
+
+import java.util.StringJoiner;
+
+public class StringJoinerExample {
+    public static void main(String[] args) {
+        // Set some data up
+        String[] wordArray = new String[]{"Hello", "World", "How", "are", "you"};
+
+        // ---- public String concat(String str)
+        String sentence = "";
+
+        // StringJoiner was added in Java 8 as well..
+        StringJoiner sj = new StringJoiner(" ");
+        for (String word : wordArray) sj.add(word);
+        sentence = sj.toString();
+        System.out.println("Our sentence using StringJoiner: " + sentence);
+
+    }
+}
